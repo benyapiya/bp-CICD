8859b4ce8a26ef5c461c8a9e88136be599f21708
i852081 <benya.piyathamsoontorn@sap.com>
2019-03-18 22:39:24 -0400

    delete method

--- vars/buildMicroService.groovy
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env groovy
-
-def call(pipelineHost) {
-  sh "container_id=$(docker ps | grep bp-microservice | awk '{ print $1 }');docker stop $container_id"
-  sh "mvn install"
-  sh "docker build -f Dockerfile -t benya/bp-microservice:${env.BRANCH_NAME} ."
-  sh "docker run -d -p 8080:8080 -e SA_LOGIC_API_URL='http://${pipelineHost}:5050' benya/bp-microservice:${env.BRANCH_NAME}"
-}
