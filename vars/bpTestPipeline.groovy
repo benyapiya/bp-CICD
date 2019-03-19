1b25ba13aa6df097d52347bd4eba5eaa72bcb23b
i852081 <benya.piyathamsoontorn@sap.com>
2019-03-18 22:04:30 -0400

    create bpTestPipeline

---
+++ vars/bpTestPipeline.groovy
@@ -0,0 +1,35 @@
+#!/usr/bin/env groovy
+/*
+pass pipeline parameters to Jenkinsfile
+bpTestPipeline (
+  pipelineHost: "ip_of_host_to_run_pipeline",
+  prodHost: "ip_of_production_host",
+  email: "your_email"
+)
+*/
+def call(Map params){
+  pipeline {
+    agent { label 'master' }
+    // Pull request has been cloned and merged by jenkins
+    environment {
+      prod_branch = 'prod'
+    }
+
+    stages {
+      stage('Init') {
+        steps {
+          script{
+            // Create PR branches for testing
+            pr_master = createPRBrach ("master")
+          }
+        }
+      }
+
+    }
+    post {
+      always {
+        emailext attachLog: true, to: '${params.email}', subject: "${env.JOB_NAME} : #${env.BUILD_NUMBER}", body: "Body"
+      }
+    }
+  }
+}
--- vars/createPRBranch.groovy
+++ vars/createPRBranch.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
-def call(repo) {
+def call() {
   sh "git checkout -B ${env.BRANCH_NAME}"
   sh "git pull ${env.BRANCH_NAME} || true"
   sh "git push ${env.BRANCH_NAME}"
