31a82a1a368ec2185d1d7ecaed3cdc5d652a60c7
i852081 <benya.piyathamsoontorn@sap.com>
2019-03-19 02:15:56 -0400

    login to docker before push

--- vars/k8sRolloutMicroservice.groovy
+++ vars/k8sRolloutMicroservice.groovy
@@ -2,6 +2,9 @@
 
 def call(){
   sh "docker build -f Dockerfile -t benya/bp-microservice ."
+  withCredentials([usernamePassword(credentialsId: 'docker_svc', passwordVariable: 'dockerPass', usernameVariable: 'dockerUser')]) {
+    sh "docker login -u ${dockerUser} -p ${dockerPass}"
+  }
   sh "docker push benya/bp-microservice"
   sh "sed -i 's/image: benya\\/bp-microservice/image: benya\\/bp-pythonservice:v0.1/' /root/.kube/bp-microservice-deployment.yaml"
   sh "kubectl apply -f /root/.kube/bp-pythonservice-deployment.yaml"
--- vars/k8sRolloutPythonservice.groovy
+++ vars/k8sRolloutPythonservice.groovy
@@ -2,6 +2,9 @@
 
 def call(){
   sh "docker build -f Dockerfile -t benya/bp-pythonservice ."
+  withCredentials([usernamePassword(credentialsId: 'docker_svc', passwordVariable: 'dockerPass', usernameVariable: 'dockerUser')]) {
+    sh "docker login -u ${dockerUser} -p ${dockerPass}"
+  }
   sh "docker push benya/bp-pythonservice"
   sh "sed -i 's/image: benya\\/bp-pythonservice/image: benya\\/bp-pythonservice:v0.1/' /root/.kube/bp-pythonservice-deployment.yaml"
   sh "kubectl apply -f /root/.kube/bp-pythonservice-deployment.yaml"
