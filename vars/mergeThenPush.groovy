6612407e8dda52a98ea17aa36e74e3f3cc148ec3
i852081 <benya.piyathamsoontorn@sap.com>
2019-03-19 01:38:07 -0400

    fix git merge

--- vars/mergeThenPush.groovy
+++ vars/mergeThenPush.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 def call(repo, toBranch) {
   withCredentials([usernamePassword(credentialsId: 'git_svc', passwordVariable: 'gitPass', usernameVariable: 'gitUser')]) {
-    sh "git checkout ${env.BRANCH_NAME}"
+    sh "git checkout ${toBranch}"
     sh "git pull https://${gitUser}:${gitPass}@${repo} ${toBranch}"
     sh "git merge ${env.BRANCH_NAME} --no-edit"
     sh "git push https://${gitUser}:${gitPass}@${repo} ${env.BRANCH_NAME}"
@@ -10,7 +10,7 @@ def call(repo, toBranch) {
 
 def call(repo, toBranch, remoteBranch) {
   withCredentials([usernamePassword(credentialsId: 'git_svc', passwordVariable: 'gitPass', usernameVariable: 'gitUser')]) {
-    sh "git checkout ${env.BRANCH_NAME}"
+    sh "git checkout ${toBranch}"
     sh "git pull https://${gitUser}:${gitPass}@${repo} ${toBranch}"
     sh "git merge ${env.BRANCH_NAME} --no-edit"
     sh "git push https://${gitUser}:${gitPass}@${repo} ${remoteBranch}"
